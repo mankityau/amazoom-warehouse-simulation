e4884a8088ee3baf1fcc53e40acb8d26ecc69b70
Yau Man Kit <Yau Man Kit>
2017-11-23 15:51:26 -0800

    Fixed bug

--- src/reality/item.cpp
+++ src/reality/item.cpp
@@ -7,7 +7,7 @@ namespace reality{
         this->symbol = symbol;
     }
 
-    Item::Item(std::string id, Location initLocation, char symbol) : Item(id, initLocation, initLocation, symbol){};
+    Item::Item(std::string id, Location initLocation, char symbol) : Item(id, initLocation, {0,0,0}, symbol){};
 
     std::string Item::getId(){
         return id;
