62671246398128cf1bc199ff587c66ad9df2a183
mankityau <david.mankit.yau@gmail.com>
2017-11-27 03:20:06 -0800

    Added gTest.

---
+++ test/reality/spacemanager_test.cpp
@@ -0,0 +1,8 @@
+#include <gtest/gtest.h>
+#include "reality/spacemanager.cpp"
+TEST(spacemanager_test, DUMPTEST){
+    reality::SpaceManager spaceManager("NAME", {0,0,0});
+    ASSERT_EQ(true, true);
+
+}
+
